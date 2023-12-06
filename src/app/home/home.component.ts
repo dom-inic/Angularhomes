9a25d55a54bf131a469e9f410500e2f6b398d7a4
dom-inic <dominicnyambane22@gmail.com>
2023-12-06 08:14:37 +0300

    housingLocation Interface

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { Housinglocation } from '../housinglocation';
 
 @Component({
   selector: 'app-home',
@@ -20,5 +21,18 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+
+  housingLocation: Housinglocation = {
+    id: 2024,
+    name: 'kilimani home',
+    city: 'Nairobi',
+    state: 'kilimani',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 50,
+    wifi: true,
+    laundry: false,
+
+  }
 
 }
