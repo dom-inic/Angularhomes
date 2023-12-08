44821df6cfb1e298ab2678e3c0cea1b8a2e4d0d2
dom-inic <dominicnyambane22@gmail.com>
2023-12-08 09:08:41 +0300

    property binding, interpolation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,7 +14,7 @@ import { Housinglocation } from '../housinglocation';
       <button class="primary" type="button">Search</button>
     </form>
     <section class="results">
-      <app-housing-location></app-housing-location>
+      <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
     </section>
   </section>
 `,
