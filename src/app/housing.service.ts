cb05aeed7b63c7d88ec811303b7cc1c605cfc7e1
dom-inic <dominicnyambane22@gmail.com>
2023-12-12 09:51:12 +0300

    angular forms

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -109,5 +109,8 @@ export class HousingService {
     return this.housingLocationList.find(housingLocation => housingLocation.id === id)
   }
 
+  submitApplication(firstName: string, lastName: string, email: string){
+    console.log(`Home application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}`)
+  }
   constructor() { }
 }
