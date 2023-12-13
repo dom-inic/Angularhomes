d2a90ee570e84b25adfa9affd78a490cc9c8f30d
dom-inic <dominicnyambane22@gmail.com>
2023-12-13 08:46:58 +0300

    serch func

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,14 +11,11 @@ import { HousingService } from '../housing.service';
   template: `
   <section>
     <form>
-      <input type="text" placeholder="Filter by city">
-      <button class="primary" type="button">Search</button>
+      <input type="text" placeholder="Filter by city" #filter>
+      <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
     </form>
     <section class="results">
-      <app-housing-location
-      *ngFor="let housingLocation of housingLocationList"
-      [housingLocation]="housingLocation">
-    </app-housing-location>
+    <app-housing-location *ngFor="let housingLocation of filteredLocationList" [housingLocation]="housingLocation"></app-housing-location>
     </section>
   </section>
 `,
@@ -27,11 +24,24 @@ import { HousingService } from '../housing.service';
 export class HomeComponent {
   readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
 
-  housingLocationList: Housinglocation[] = []
-  housingService: HousingService = inject(HousingService)
+  housingLocationList: Housinglocation[] = [];
+  housingService: HousingService = inject(HousingService);
+  filteredLocationList : Housinglocation[] = [];
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
 
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
   }
 
 }
